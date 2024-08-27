99ca0911c7228ee3b493c491d88c2c2280381d38
smfulton <sfulton1996@gmail.com>
2024-08-26 20:20:54 -0500

    Completed ability to add notes and submit to boards.

--- part3/studio/src/components/BoardAssignment.jsx
+++ part3/studio/src/components/BoardAssignment.jsx
@@ -1,15 +1,26 @@
 import { useState } from 'react';
 
 export default function BoardAssignment () {
-   const boards = ["Sushi Lovers","Asian Cuisine",""];
+   let [boardName,setBoardName] = useState("no boards yet!");
+   const boards = [
+      {label: "Sushi Lovers", value: "sushi-lovers"},
+      {label: "Asian Cuisine", value: "asain-cuisine"},
+      {label: "Mostly Rice", value: "mostly-rice"}
+   ];
 
    const handleChange = (event) => {
+      setBoardName(event.target.value);
    }
 
    return (
       <div style={{paddingTop: "50px"}}>
       <label>Save to Board: </label>
       <select value={boardName} onChange={handleChange}>
+         {boards.map((board, index) => {
+            return (
+               <option key={index} value={board.value}>{board.label}</option>
+            );
+         })}
       </select>
 
       <p>Saved to {boardName}!</p>
--- part3/studio/src/components/StatusChange.jsx
+++ part3/studio/src/components/StatusChange.jsx
@@ -2,10 +2,16 @@ import { useState } from 'react';
 
 export default function StatusChange () {
 
+   let [notes,setNotes] = useState("");
+   let [recipeStatus,setRecipeStatus] = useState(false);
+
    const handleChange = (event) => {
+      setNotes(event.target.value);
    }
 
    const handleSubmit = (event) => {
+      event.preventDefault();
+      setRecipeStatus(true);
    }
 
    return (
@@ -16,9 +22,9 @@ export default function StatusChange () {
             <input type="submit" />
          </form>
 
-         <p>My Recipe Notes aren't here!</p>
+         <p>{notes === "" ? "My Recipe Notes aren't here!" : notes}</p>
 
-         <p>I have not tried this recipe!</p>
+         <p>{recipeStatus ? "I have tried this recipe!" : "I have not tried this recipe!"}</p>
       </div>
    );
 }
